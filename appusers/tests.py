bb9d018f9ea31d94067fbe1abfbe3d5360f037eb
Kurz <kurz956kk@gmail.com>
2024-02-26 17:04:16 +0500

    bd appusers tests added

--- appusers/tests.py
+++ appusers/tests.py
@@ -1,3 +1,49 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class RegisterUserTestCase(TestCase):
+    def setUp(self):
+        self.data = {
+            'username': 'user1',
+            'email': 'user1@mail.ru',
+            'name': 'Вася',
+            'surname': 'Пупкин',
+            'password1': '1',
+            'password2': '1',
+        }
+
+    def test_form_registration_get(self):
+        path = reverse('users:register')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'appusers/register.html')
+
+    def test_user_registration_success(self):
+        user_model = get_user_model()
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(user_model.objects.filter(username=self.data['username']).exists())
+
+    def test_user_registration_password_error(self):
+        self.data['password2'] = '2'
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Введенные пароли не совпадают.', html=True)
+
+    def test_user_registration_exists_error(self):
+        user_model = get_user_model()
+        user_model.objects.create(username=self.data['username'])
+
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
+
